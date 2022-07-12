e907f9e747bd33cdb8591a89538aa1500b00772c
Ellie <EllieRHudson@gmail.com>
2022-07-12 16:00:19 +0100

    Grid

---
+++ src/Grid.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
+
+const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+});
+
+export default function ComplexGrid() {
+    return (
+            <Paper
+              sx={{
+                p: 2,
+                margin: 'auto',
+                maxWidth: 500,
+                flexGrow: 1,
+                backgroundColor: (theme) =>
+                  theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+              }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm container>
+                  <Grid item xs container direction="column" spacing={2}>
+                    <Grid item xs>
+                      <Typography gutterBottom variant="subtitle1" component="div">
+                        This is a test box
+                      </Typography>
+                      <Typography variant="body2" gutterBottom>
+                        I am using mui/material to create a grid
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        12/07/2022
+                      </Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography sx={{ cursor: 'pointer' }} variant="body2">
+                        Remove
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="subtitle1" component="div">
+                      Top Right
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Paper>
+    );
+}
